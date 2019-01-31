d876a924530335763a33c7e5b97d119c78ca8be6
anderson9212 <anderson_fos@hotmail.com>
2019-01-31 13:50:03 -0300

    melhorias

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,7 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.views.generic.edit import CreateView, UpdateView
-
 from apps.empresas.models import Empresa
 
 
